e404a251021b6bf7179911b623a3f5e915ac2ebe
Tim Markowski <timmarkow@gmail.com>
2011-10-18 21:09:26 -0700

    update root

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Twitter::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "posts#index"
+   root :to => "posts#index"
 
   # See how all your routes lay out with "rake routes"
 
